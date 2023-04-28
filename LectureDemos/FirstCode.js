446bdf5b9bf9e211f7abbed63972836f4d49b12d
Theo2039 <theodorewalker61@yahoo.com>
2023-04-27 21:59:33 -0400

    update

---
+++ LectureDemos/FirstCode.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var firstName;
+
+firstName = "Theo";
+
+var city;
+city = "Boston";
+
+var state
+state = "Massachusetts";
+
+var greeting;
+
+greeting = ("Your name is " + firstName);
+
+var birthPlace
+birthPlace = "I was born in " + city + state
+
+console.log(greeting);
+console.log(birthPlace)
